d49f84a62a06ef764ce83d60c1be0c08abd807c4
Devin Ulrey <Devin.Ulrey@gmail.com>
2024-09-30 20:03:34 -0400

    - Sending buttons that don't exist will now disconnect the Player

--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -64,6 +64,7 @@ namespace Server.Misc
 			builder.Append("- Gather - The 'Resources' server setting is now limited by the amount of resources in the harvest bank<br>");
 			builder.Append("- Gather - Drop harvested items to ground when backpack is full<br>");
 			builder.Append("- Gump - Alien Players who use Tithe to pay for a res from another player are no longer double penalized<br>");
+			builder.Append("- Gump - Sending buttons that don't exist will now disconnect the Player<br>");
 			builder.Append("- Item - Added missing Powerscrolls (Begging, Camping, Forensics, Mercantile, Tasting)<br>");
 			builder.Append("- Item - Lucky horse shoe now work on Instruments and Quivers<br>");
 			builder.Append("- Misc - Set Map when [scan players<br>");
