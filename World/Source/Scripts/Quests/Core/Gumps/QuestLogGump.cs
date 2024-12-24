a72db8b2958a7f991ae955124345edbc41e8d162
Devin Ulrey <Devin.Ulrey@gmail.com>
2024-12-24 08:46:51 -0500

    - Add Toggle Quest Item button to Quest Log

--- World/Source/Scripts/Quests/Core/Gumps/QuestLogGump.cs
+++ World/Source/Scripts/Quests/Core/Gumps/QuestLogGump.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using Server.Engines.MLQuests.Objectives;
 using Server.Gumps;
 using Server.Mobiles;
 using Server.Network;
@@ -43,7 +45,8 @@ namespace Server.Engines.MLQuests.Gumps
 
 				for (int i = 0; i < instances.Count; ++i)
 				{
-					if (instances[i].Failed)
+					var questInstance = instances[i];
+					if (questInstance.Failed)
 					{
 						numberColor = 0x3C00;
 						stringColor = 0x7B0000;
@@ -54,29 +57,46 @@ namespace Server.Engines.MLQuests.Gumps
 						stringColor = BaseQuestGump.COLOR_HTML;
 					}
 
-					TextDefinition.AddHtmlText(this, 98, 140 + 21 * i, 270, 21, instances[i].Quest.Title, false, false, numberColor, stringColor);
-					AddButton(368, 140 + 21 * i, 0x26B0, 0x26B1, 6 + i, GumpButtonType.Reply, 1);
+					TextDefinition.AddHtmlText(this, 98, 140 + 21 * i, 270, 21, questInstance.Quest.Title, false, false, numberColor, stringColor);
+					AddButton(368, 140 + 21 * i, 0x26B0, 0x26B1, 6 + 1 + i * 1000, GumpButtonType.Reply, 1); // Arrow
+					if (!questInstance.IsCompleted()
+						&& !questInstance.Failed
+						&& questInstance.Objectives.Any(x => x is CollectObjectiveInstance || x is CraftObjectiveInstance))
+					{
+						AddButton(368 + 21 + 5, 140 + 21 * i, 13006, 13006, 6 + 2 + i * 1000, GumpButtonType.Reply, 1); // Crosshair
+					}
 				}
 			}
 		}
 
 		public override void OnResponse(NetState sender, RelayInfo info)
 		{
-			if (info.ButtonID < 6)
-				return;
+			if (info.ButtonID < 6) return;
 
 			MLQuestContext context = MLQuestSystem.GetContext(m_Owner);
-
-			if (context == null)
-				return;
+			if (context == null) return;
 
 			List<MLQuestInstance> instances = context.QuestInstances;
-			int index = info.ButtonID - 6;
+			int buttonId = info.ButtonID - 6;
+			int index = buttonId / 1000;
+			if (index >= instances.Count) return;
 
-			if (index >= instances.Count)
-				return;
+			var questInstance = instances[index];
+			var actionId = buttonId - index * 1000;
+			switch (actionId)
+			{
+				case 1: // Get Info
+					sender.Mobile.SendGump(new QuestLogDetailedGump(questInstance, m_CloseGumps));
+					break;
 
-			sender.Mobile.SendGump(new QuestLogDetailedGump(instances[index], m_CloseGumps));
+				case 2: // Toggle Quest Items
+					var player = (PlayerMobile)sender.Mobile;
+					player.ToggleQuestItem();
+					break;
+
+				default:
+					break;
+			}
 		}
 	}
 }
