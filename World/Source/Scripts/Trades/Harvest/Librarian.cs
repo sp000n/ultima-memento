7525c730bd51437aedde1122afade2481b995d7e
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-01-01 08:07:52 -0500

    - Update cliloc for searching bookshelves

--- World/Source/Scripts/Trades/Harvest/Librarian.cs
+++ World/Source/Scripts/Trades/Harvest/Librarian.cs
@@ -57,8 +57,8 @@ namespace Server.Engines.Harvest
 			library.EffectCounts = new int[]{ 1 };
 			library.EffectDelay = TimeSpan.FromSeconds( 0.0 );
 			library.EffectSoundDelay = TimeSpan.FromSeconds( 0.1 );
-			library.NoResourcesMessage = 501756; // Nothing worth taking.
-			library.FailMessage = 501756; // Nothing worth taking.
+			library.NoResourcesMessage = 1060530; // The shelf is completely empty.
+			library.FailMessage = 1060531; // You shuffle around and find nothing of interest.
 			library.OutOfRangeMessage = 500446; // That is too far away.
 			library.PackFullMessage = 500720; // You don't have enough room in your backpack!
 			library.ToolBrokeMessage = 1044038; // You broke your tool.
