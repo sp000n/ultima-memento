e190b3c415d835c96ac0dcf25ceddb358994cc66
WarrentyExpired <micah.zellers@gmail.com>
2025-02-10 16:47:34 -0500

    Revert "# Added script to compile for Linux"
    This reverts commit 0fbe17690e16b9946de02d0f55042f9eb162c05b.

--- World/Source/Tools/compile-world-linux.sh
+++
@@ -1,2 +0,0 @@
-mcs -optimize+ -unsafe -t:exe -out:WorldLinux.exe -win32icon:../System/icon.ico -nowarn:219,414 -d:NEWTIMERS -d:NEWPARENT -d:MONO -recurse:../System/*.cs -main:Server.Core
-mv ./WorldLinux.exe ../../
