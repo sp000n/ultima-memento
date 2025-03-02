ad64dc1d6d4dc896a4de1ca119b82de09ec56c05
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-03-02 00:01:05 -0500

    - Add Red color

--- World/Source/Scripts/System/Gumps/GumpColors.cs
+++ World/Source/Scripts/System/Gumps/GumpColors.cs
@@ -4,6 +4,7 @@ namespace Server.Gumps
     public class HtmlColors // RGB888
     {
         public const int OFFWHITE = 0xf7fbde;
+		public const int RED = 0xff0000;
     }
 
     public class LabelColors // Hue from files / in-game
