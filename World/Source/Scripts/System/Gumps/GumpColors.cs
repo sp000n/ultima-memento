80242753a4cf64d46c631635521bd59517f61525
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-03-03 18:47:50 -0500

    -

--- World/Source/Scripts/System/Gumps/GumpColors.cs
+++ World/Source/Scripts/System/Gumps/GumpColors.cs
@@ -1,15 +1,15 @@
 
 namespace Server.Gumps
 {
-    public class HtmlColors // RGB888
-    {
-        public const int OFFWHITE = 0xf7fbde;
-		    public const int RED = 0xff0000;
-        public const int WHITE = 0xffffff;
-    }
+  public class HtmlColors // RGB565 - (Top Left) https://rgbcolorpicker.com/565
+  {
+    public const int OFFWHITE = 0xf7fbde;
+    public const int RED = 0xff0000;
+    public const int WHITE = 0xffffff;
+  }
 
-    public class LabelColors // Hue from files / in-game
-    {
-        public const int OFFWHITE = 1918; // RGB888
-    }
+  public class LabelColors // Hue from files / in-game
+  {
+    public const int OFFWHITE = 1918; // RGB888
+  }
 }
