db30ad39a1790b3f6e896615189bed0c723d8e88
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-03-04 21:29:50 -0500

    - Consolidate pugilist gloves

--- World/Source/Scripts/Items/Weapons/Hands/PugilistGlove.cs
+++ World/Source/Scripts/Items/Weapons/Hands/PugilistGlove.cs
@@ -8,8 +8,7 @@ namespace Server.Items
 	{
 	}
 
-	[FlipableAttribute( 0x1450, 0x1450 )]
-	public class PugilistGlove : BaseWeapon, IPugilistGlove
+	public abstract class PugilistGloveWeapon : BaseWeapon, IPugilistGlove
 	{
 		public override WeaponAbility PrimaryAbility{ get{ return WeaponAbility.Disarm; } }
 		public override WeaponAbility SecondaryAbility{ get{ return WeaponAbility.ParalyzingBlow; } }
@@ -35,6 +34,37 @@ namespace Server.Items
 
 		public override CraftResource DefaultResource{ get{ return CraftResource.RegularLeather; } }
 
+		public PugilistGloveWeapon( int itemID ) : base( itemID )
+		{
+			
+		}
+
+		public PugilistGloveWeapon( Serial serial ) : base( serial )
+		{
+		}
+
+		public override void AddNameProperties( ObjectPropertyList list )
+		{
+			base.AddNameProperties( list );
+			list.Add( 1049644, "Cannot be used with hand-held weapons" );
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+			writer.Write( (int) 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+			int version = reader.ReadInt();
+		}
+	}
+
+	[FlipableAttribute( 0x1450, 0x1450 )]
+	public class PugilistGlove : PugilistGloveWeapon
+	{
 		[Constructable]
 		public PugilistGlove() : base( 0x1450 )
 		{
@@ -43,12 +73,6 @@ namespace Server.Items
 			Hue = Utility.RandomColor(0);
 			Layer = Layer.OneHanded;
 			Attributes.SpellChanneling = 1;
-		}
-
-		public override void AddNameProperties( ObjectPropertyList list )
-		{
-			base.AddNameProperties( list );
-			list.Add( 1049644, "Cannot be used with hand-held weapons" );
 		}
 
 		public PugilistGlove( Serial serial ) : base( serial )
@@ -67,33 +91,10 @@ namespace Server.Items
 			int version = reader.ReadInt();
 		}
 	}
+
 	[FlipableAttribute( 0x13C6, 0x13C6 )]
-	public class PugilistGloves : BaseWeapon, IPugilistGlove
+	public class PugilistGloves : PugilistGloveWeapon
 	{
-		public override WeaponAbility PrimaryAbility{ get{ return WeaponAbility.Disarm; } }
-		public override WeaponAbility SecondaryAbility{ get{ return WeaponAbility.ParalyzingBlow; } }
-		public override WeaponAbility ThirdAbility{ get{ return WeaponAbility.WhirlwindAttack; } }
-		public override WeaponAbility FourthAbility{ get{ return WeaponAbility.FistsOfFury; } }
-		public override WeaponAbility FifthAbility{ get{ return WeaponAbility.DeathBlow; } }
-
-		public override int AosStrengthReq{ get{ return 20; } }
-		public override int AosMinDamage{ get{ return 8; } }
-		public override int AosMaxDamage{ get{ return 10; } }
-		public override int AosSpeed{ get{ return 2; } }
-		public override float MlSpeed{ get{ return 2.00f; } }
-
-		public override int DefHitSound{ get{ return 0x13D; } }
-		public override int DefMissSound{ get{ return 0x238; } }
-
-		public override int InitMinHits{ get{ return 31; } }
-		public override int InitMaxHits{ get{ return 50; } }
-
-		public override SkillName DefSkill{ get{ return SkillName.FistFighting; } }
-		public override WeaponType DefType{ get{ return WeaponType.Fists; } }
-		public override WeaponAnimation DefAnimation{ get{ return WeaponAnimation.Punching; } }
-
-		public override CraftResource DefaultResource{ get{ return CraftResource.RegularLeather; } }
-
 		[Constructable]
 		public PugilistGloves() : base( 0x13C6 )
 		{
@@ -102,12 +103,6 @@ namespace Server.Items
 			Hue = Utility.RandomColor(0);
 			Layer = Layer.OneHanded;
 			Attributes.SpellChanneling = 1;
-		}
-
-		public override void AddNameProperties( ObjectPropertyList list )
-		{
-			base.AddNameProperties( list );
-			list.Add( 1049644, "Cannot be used with hand-held weapons" );
 		}
 
 		public PugilistGloves( Serial serial ) : base( serial )
