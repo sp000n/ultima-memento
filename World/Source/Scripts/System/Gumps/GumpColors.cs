c06dd752cbf068dcc1cfe89795368c07d201ba79
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-03-07 20:00:41 -0500

    - Add new colors

--- World/Source/Scripts/System/Gumps/GumpColors.cs
+++ World/Source/Scripts/System/Gumps/GumpColors.cs
@@ -3,6 +3,10 @@ namespace Server.Gumps
 {
   public class HtmlColors // RGB565 - (Top Left) https://rgbcolorpicker.com/565
   {
+    public const int COOL_BLUE = 0x79bfdc;
+    public const int COOL_GREEN = 0x83b587;
+    public const int KHAKI = 0xdcb179;
+    public const int LIGHT_PINK = 0xe59de2;
     public const int OFFWHITE = 0xf7fbde;
     public const int RED = 0xff0000;
     public const int WHITE = 0xffffff;
