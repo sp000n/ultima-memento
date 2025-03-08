d4478e9a97fb6eba98b88b8e0dfb1f0ef7fcec81
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-03-08 09:27:12 -0500

    - Traits can now be spent

--- World/Source/Scripts/System/Skills/Druidism.cs
+++ World/Source/Scripts/System/Skills/Druidism.cs
@@ -5,6 +5,7 @@ using Server.Targeting;
 using Server.Misc;
 using Server.Items;
 using Server.Network;
+using Custom.Jerbal.Jako;
 
 namespace Server.SkillHandlers
 {
@@ -107,8 +108,22 @@ namespace Server.SkillHandlers
 
 	public class DruidismGump : Gump
 	{
+		private enum Actions
+		{
+			HitPoints = 1,
+			StaminaPoints,
+			ManaPoints,
+			ResistPhysical,
+			ResistFire,
+			ResistCold,
+			ResistPoison,
+			ResistEnergy,
+		}
+
 		private readonly int m_Book; 
 		private readonly int m_Color;
+		private readonly int m_Source;
+		private readonly BaseCreature m_Creature;
 
 		private static string FormatSkill( BaseCreature c, SkillName name )
 		{
@@ -259,6 +274,17 @@ namespace Server.SkillHandlers
 			Mobile from = state.Mobile; 
 			if ( m_Book > 0 ){ from.SendSound( 0x55 ); }
 			else { from.SendSound( 0x0F9 ); }
+
+			if (m_Source == 0)
+			{
+				if (info.ButtonID < 1) return;
+
+				Actions action = (Actions)info.ButtonID;
+				if (CanAddBonus(action))
+					GetAttribute(action).ApplyBonus(m_Creature);
+
+				from.SendGump(new DruidismGump(from, m_Creature, m_Source));
+			}
 		}
 
 		public DruidismGump( Mobile from, BaseCreature c, int source ) : base( 50, 50 )
@@ -269,6 +295,9 @@ namespace Server.SkillHandlers
 			this.Resizable=false;
 
 			AddPage(0);
+
+			m_Source = source;
+			m_Creature = c;
 
 			const int COLUMN_ONE_START = 22;
 			const int COLUMN_TWO_START = 252;
@@ -285,27 +314,27 @@ namespace Server.SkillHandlers
 			m_Color = HtmlColors.COOL_BLUE;
 			string title = "MONSTER MANUAL";
 
-			if ( source == 1 || source == 2 ){ m_Book = 1; }
+			if ( m_Source == 1 || m_Source == 2 ){ m_Book = 1; }
 
-			if ( source == 2 )
+			if ( m_Source == 2 )
 			{
 				img = 11417;
 				m_Color = HtmlColors.KHAKI;
 				title = "PLAYERS HANDBOOK";
 			}
-			else if ( source == 0 )
+			else if ( m_Source == 0 )
 			{
 				img = 11418;
 				m_Color = HtmlColors.COOL_GREEN;
 				title = "ANIMAL LORE";
 			}
-			else if ( source == 3 )
+			else if ( m_Source == 3 )
 			{
 				img = 11419;
 				m_Color = HtmlColors.LIGHT_PINK;
 				title = "DIVINATION";
 			}
-			else if ( source == 4 )
+			else if ( m_Source == 4 )
 			{
 				img = 11419;
 				m_Color = HtmlColors.LIGHT_PINK;
@@ -314,8 +343,8 @@ namespace Server.SkillHandlers
 
 			AddImage(1, 1, img, Server.Misc.PlayerSettings.GetGumpHue( from ));
 
-			string name = c.Name;
-				if ( c.Title != "" && c.Title != null ){ name = name + " " + c.Title; }
+			string name = m_Creature.Name;
+				if ( m_Creature.Title != "" && m_Creature.Title != null ){ name = name + " " + m_Creature.Title; }
 
 			TextDefinition.AddHtmlText(this, 14, 15, 167, 20, title, false, false, m_Color, m_Color);
 			TextDefinition.AddHtmlText(this, 179, 15, 344, 20, string.Format("<CENTER>{0}</CENTER>", name.ToUpper()), false, false, m_Color, m_Color);
@@ -328,19 +357,19 @@ namespace Server.SkillHandlers
 			x = COLUMN_ONE_START;
 			y = Y_START;
 			AddRow(x, ref y, "INFORMATION", "");
-			AddInformationSection(x + SECTION_INDENT, ref y, c, source);
+			AddInformationSection(x + SECTION_INDENT, ref y);
 
-			if ( c.Tamable )
+			if ( m_Creature.Tamable )
 			{
 				y += MARGIN_TOP;
 				AddRow(x, ref y, "TAME", "");
-				AddPetSection(x + SECTION_INDENT, ref y, c, source);
+				AddPetSection(x + SECTION_INDENT, ref y);
 
-				if ( source == 0 && c.MinTameSkill > 0 )
+				if ( source == 0 && m_Creature.MinTameSkill > 0 )
 				{
 					y += MARGIN_TOP;
 					AddRow(x, ref y, "FAVORITE FOOD", "");
-					AddFoodSection(x + SECTION_INDENT, ref y, c, source);
+					AddFoodSection(x + SECTION_INDENT, ref y);
 				}
 			}
 
@@ -349,33 +378,42 @@ namespace Server.SkillHandlers
 			x = COLUMN_TWO_START;
 			y = Y_START;
 			AddRow(x, ref y, "DAMAGE", "");
-			AddDamageSection(x + SECTION_INDENT, ref y, c, source);
+			AddDamageSection(x + SECTION_INDENT, ref y);
 
 			y += MARGIN_TOP;
 			AddRow(x, ref y, "COMBAT RATINGS", "");
-			AddCombatRatingsSection(x + SECTION_INDENT, ref y, c, source);
+			AddCombatRatingsSection(x + SECTION_INDENT, ref y);
 
 			y += MARGIN_TOP;
 			AddRow(x, ref y, "LORE & KNOWLEDGE", "");
-			AddLoreAndKnowledgeSection(x + SECTION_INDENT, ref y, c, source);
+			AddLoreAndKnowledgeSection(x + SECTION_INDENT, ref y);
 
 			///////////////////////////////////////////////////////////////////////////////////
 
 			x = COLUMN_THREE_START;
 			y = Y_START;
 			AddRow(x, ref y, "RESISTANCE", "");
-			AddResistanceSection(x + SECTION_INDENT, ref y, c, source);
+			AddResistanceSection(x + SECTION_INDENT, ref y);
 
 			y += MARGIN_TOP;
 			AddRow(x, ref y, "STATS", "");
-			AddStatsSection(x + SECTION_INDENT, ref y, c, source);
+			AddStatsSection(x + SECTION_INDENT, ref y);
 		}
 
-		private void AddRow(int x, ref int y, string label, string value)
+		private void AddRow(int x, ref int y, string label, string value, int? buttonId = null)
 		{
+			if (m_Source == 0 && buttonId.HasValue)
+			{
+				if (CanAddBonus((Actions)buttonId))
+				{
+					const int PLUS_ICON = 13007;
+					AddButton(x - 9, y + 5, PLUS_ICON, PLUS_ICON, buttonId.Value, GumpButtonType.Reply, 0);
+				}
+			}
+
 			const int TOTAL_WIDTH = 200;
 			int leftWidth = TOTAL_WIDTH;
-			if (value.Length == 0){}
+			if (value.Length == 0) { }
 			else if (label.Length < value.Length) leftWidth = 50;
 			else if (value.Length < label.Length) leftWidth = 125;
 
@@ -386,90 +424,122 @@ namespace Server.SkillHandlers
 			y += 20;
 		}
 
-        private void AddStatsSection( int x, ref int y, BaseCreature c, int source)
+		private bool CanAddBonus(Actions action)
 		{
-			AddRow(x, ref y, "Hits", FormatNumber( c.Hits ) + "/" + FormatNumber( c.HitsMax ));
-			AddRow(x, ref y, "Stamina", FormatNumber( c.Stam ) + "/" + FormatNumber( c.StamMax ));
-			AddRow(x, ref y, "Mana", FormatNumber( c.Mana ) + "/" + FormatNumber( c.ManaMax ));
-			AddRow(x, ref y, "Strength", FormatNumber( c.Str ));
-			AddRow(x, ref y, "Dexterity", FormatNumber( c.Dex ));
-			AddRow(x, ref y, "Intelligence", FormatNumber( c.Int ));
+			JakoBaseAttribute attribute = GetAttribute(action);
+
+			return attribute != null && attribute.CanAfford(m_Creature) && attribute.CanAddBonus(m_Creature);
 		}
 
-        private void AddDamageSection( int x, ref int y, BaseCreature c, int source)
+		private JakoBaseAttribute GetAttribute(Actions action)
 		{
-			AddRow(x, ref y, "Physical", FormatPercent( c.PhysicalDamage ));
-			AddRow(x, ref y, "Fire", FormatPercent( c.FireDamage ));
-			AddRow(x, ref y, "Cold", FormatPercent( c.ColdDamage ));
-			AddRow(x, ref y, "Poison", FormatPercent( c.PoisonDamage ));
-			AddRow(x, ref y, "Energy", FormatPercent( c.EnergyDamage ));
-			AddRow(x, ref y, "Base Damage", c.DamageMin + " - " + c.DamageMax);
+			JakoAttributesEnum jakoEnum = GetJakoAttribute(action);
+
+            return jakoEnum != JakoAttributesEnum.None ? m_Creature.JakoAttributes.GetAttribute(jakoEnum) : null;
+        }
+
+        private JakoAttributesEnum GetJakoAttribute(Actions action)
+		{
+			switch(action)
+			{
+				case Actions.HitPoints: return JakoAttributesEnum.Hits;
+				case Actions.StaminaPoints: return JakoAttributesEnum.Stam;
+				case Actions.ManaPoints: return JakoAttributesEnum.Mana;
+				case Actions.ResistPhysical: return JakoAttributesEnum.BonusPhysResist;
+				case Actions.ResistFire: return JakoAttributesEnum.BonusFireResist;
+				case Actions.ResistCold: return JakoAttributesEnum.BonusColdResist;
+				case Actions.ResistPoison: return JakoAttributesEnum.BonusPoisResist;
+				case Actions.ResistEnergy: return JakoAttributesEnum.BonusEnerResist;
+			}
+
+			return JakoAttributesEnum.None;
 		}
 
-        private void AddResistanceSection( int x, ref int y, BaseCreature c, int source)
+        private void AddStatsSection( int x, ref int y )
 		{
-			AddRow(x, ref y, "Physical", FormatPercent( c.PhysicalResistance ));
-			AddRow(x, ref y, "Fire", FormatPercent( c.FireResistance ));
-			AddRow(x, ref y, "Cold", FormatPercent( c.ColdResistance ));
-			AddRow(x, ref y, "Poison", FormatPercent( c.PoisonResistance ));
-			AddRow(x, ref y, "Energy", FormatPercent( c.EnergyResistance ));
+			AddRow(x, ref y, "Hits", FormatNumber( m_Creature.Hits ) + "/" + FormatNumber( m_Creature.HitsMax ), (int)Actions.HitPoints);
+			AddRow(x, ref y, "Stamina", FormatNumber( m_Creature.Stam ) + "/" + FormatNumber( m_Creature.StamMax ), (int)Actions.StaminaPoints);
+			AddRow(x, ref y, "Mana", FormatNumber( m_Creature.Mana ) + "/" + FormatNumber( m_Creature.ManaMax ), (int)Actions.ManaPoints);
+			AddRow(x, ref y, "Strength", FormatNumber( m_Creature.Str ));
+			AddRow(x, ref y, "Dexterity", FormatNumber( m_Creature.Dex ));
+			AddRow(x, ref y, "Intelligence", FormatNumber( m_Creature.Int ));
 		}
 
-        private void AddCombatRatingsSection( int x, ref int y, BaseCreature c, int source)
+        private void AddDamageSection( int x, ref int y )
 		{
-			AddRow(x, ref y, "Anatomy", FormatTalent( c.Skills[SkillName.Anatomy].Value ));
-			AddRow(x, ref y, "Magic Resist", FormatTalent( c.Skills[SkillName.MagicResist].Value ));
-			AddRow(x, ref y, "Poisoning", FormatTalent( c.Skills[SkillName.Poisoning].Value ));
-			AddRow(x, ref y, "Tactics", FormatTalent( c.Skills[SkillName.Tactics].Value ));
+			AddRow(x, ref y, "Physical", FormatPercent( m_Creature.PhysicalDamage ));
+			AddRow(x, ref y, "Fire", FormatPercent( m_Creature.FireDamage ));
+			AddRow(x, ref y, "Cold", FormatPercent( m_Creature.ColdDamage ));
+			AddRow(x, ref y, "Poison", FormatPercent( m_Creature.PoisonDamage ));
+			AddRow(x, ref y, "Energy", FormatPercent( m_Creature.EnergyDamage ));
+			AddRow(x, ref y, "Base Damage", m_Creature.DamageMin + " - " + m_Creature.DamageMax);
+		}
+
+        private void AddResistanceSection( int x, ref int y )
+		{
+			AddRow(x, ref y, "Physical", FormatPercent( m_Creature.PhysicalResistance ), (int)Actions.ResistPhysical);
+			AddRow(x, ref y, "Fire", FormatPercent( m_Creature.FireResistance ), (int)Actions.ResistFire);
+			AddRow(x, ref y, "Cold", FormatPercent( m_Creature.ColdResistance ), (int)Actions.ResistCold);
+			AddRow(x, ref y, "Poison", FormatPercent( m_Creature.PoisonResistance ), (int)Actions.ResistPoison);
+			AddRow(x, ref y, "Energy", FormatPercent( m_Creature.EnergyResistance ), (int)Actions.ResistEnergy);
+		}
+
+        private void AddCombatRatingsSection( int x, ref int y )
+		{
+			AddRow(x, ref y, "Anatomy", FormatTalent( m_Creature.Skills[SkillName.Anatomy].Value ));
+			AddRow(x, ref y, "Magic Resist", FormatTalent( m_Creature.Skills[SkillName.MagicResist].Value ));
+			AddRow(x, ref y, "Poisoning", FormatTalent( m_Creature.Skills[SkillName.Poisoning].Value ));
+			AddRow(x, ref y, "Tactics", FormatTalent( m_Creature.Skills[SkillName.Tactics].Value ));
 			
 			string skill;
-			if ( source == 2 )
-				skill = FormatFight( c );
-			else if ( source == 0 )
-				skill = FormatTalent( c.Skills[SkillName.FistFighting].Value );
-			else if ( source == 3 )
-				skill = FormatTalent( c.Skills[SkillName.FistFighting].Value );
-			else if ( source == 4 )
-				skill = FormatFight( c );
+			if ( m_Source == 2 )
+				skill = FormatFight( m_Creature );
+			else if ( m_Source == 0 )
+				skill = FormatTalent( m_Creature.Skills[SkillName.FistFighting].Value );
+			else if ( m_Source == 3 )
+				skill = FormatTalent( m_Creature.Skills[SkillName.FistFighting].Value );
+			else if ( m_Source == 4 )
+				skill = FormatFight( m_Creature );
 			else
-				skill = FormatTalent( c.Skills[SkillName.FistFighting].Value );
+				skill = FormatTalent( m_Creature.Skills[SkillName.FistFighting].Value );
 
 			AddRow(x, ref y, "Combat Skill", skill);
 		}
 
-        private void AddLoreAndKnowledgeSection( int x, ref int y, BaseCreature c, int source)
+        private void AddLoreAndKnowledgeSection( int x, ref int y )
 		{
-			AddRow(x, ref y, "Magery", FormatTalent( c.Skills[SkillName.Magery].Value ));
-			AddRow(x, ref y, "Meditation", FormatTalent( c.Skills[SkillName.Meditation].Value ));
-			AddRow(x, ref y, "Psychology", FormatTalent( c.Skills[SkillName.Psychology].Value ));
+			AddRow(x, ref y, "Magery", FormatTalent( m_Creature.Skills[SkillName.Magery].Value ));
+			AddRow(x, ref y, "Meditation", FormatTalent( m_Creature.Skills[SkillName.Meditation].Value ));
+			AddRow(x, ref y, "Psychology", FormatTalent( m_Creature.Skills[SkillName.Psychology].Value ));
 		}
 
-        private void AddPetSection( int x, ref int y, BaseCreature c, int source)
+        private void AddPetSection( int x, ref int y )
 		{
-			if ( c.ControlMaster == null )
+			if ( m_Creature.ControlMaster == null )
 			{
-				AddRow(x, ref y, "Max Pet Level", c.MaxLevel.ToString());
+				AddRow(x, ref y, "Max Pet Level", m_Creature.MaxLevel.ToString());
 			}
 			else
 			{
-				AddRow(x, ref y, "Pet Level", c.Level + "/" + c.MaxLevel);
-				AddRow(x, ref y, "Experience", c.Experience.ToString());
-				AddRow(x, ref y, "Experience Needed", c.ExpToNextLevel.ToString());
+				AddRow(x, ref y, "Pet Level", m_Creature.Level + "/" + m_Creature.MaxLevel);
+				AddRow(x, ref y, "Traits Available", m_Creature.Traits.ToString());
+				AddRow(x, ref y, "Experience Earned", m_Creature.Experience.ToString());
+				AddRow(x, ref y, "Experience Needed", m_Creature.ExpToNextLevel.ToString());
 			}
 
 			string packInstinct = "None";
-			if ( (c.PackInstinct & PackInstinct.Canine) != 0 ) packInstinct = "Canine";
-			else if ( (c.PackInstinct & PackInstinct.Ostard) != 0 ) packInstinct = "Ostard";
-			else if ( (c.PackInstinct & PackInstinct.Feline) != 0 ) packInstinct = "Feline";
-			else if ( (c.PackInstinct & PackInstinct.Arachnid) != 0 ) packInstinct = "Arachnid";
-			else if ( (c.PackInstinct & PackInstinct.Daemon) != 0 ) packInstinct = "Daemon";
-			else if ( (c.PackInstinct & PackInstinct.Bear) != 0 ) packInstinct = "Bear";
-			else if ( (c.PackInstinct & PackInstinct.Equine) != 0 ) packInstinct = "Equine";
-			else if ( (c.PackInstinct & PackInstinct.Bull) != 0 ) packInstinct = "Bull";
+			if ( ( m_Creature.PackInstinct & PackInstinct.Canine) != 0 ) packInstinct = "Canine";
+			else if ( ( m_Creature.PackInstinct & PackInstinct.Ostard) != 0 ) packInstinct = "Ostard";
+			else if ( ( m_Creature.PackInstinct & PackInstinct.Feline) != 0 ) packInstinct = "Feline";
+			else if ( ( m_Creature.PackInstinct & PackInstinct.Arachnid) != 0 ) packInstinct = "Arachnid";
+			else if ( ( m_Creature.PackInstinct & PackInstinct.Daemon) != 0 ) packInstinct = "Daemon";
+			else if ( ( m_Creature.PackInstinct & PackInstinct.Bear) != 0 ) packInstinct = "Bear";
+			else if ( ( m_Creature.PackInstinct & PackInstinct.Equine) != 0 ) packInstinct = "Equine";
+			else if ( ( m_Creature.PackInstinct & PackInstinct.Bull) != 0 ) packInstinct = "Bull";
 			AddRow(x, ref y, "Pack Instinct", packInstinct);
 
 			string loyalty = "Wild";
-			int loyal = 1 + (c.Loyalty / 10);
+			int loyal = 1 + ( m_Creature.Loyalty / 10);
 			switch ( loyal ) 
 			{
 				case 1: loyalty = "Confused"; break;
@@ -488,58 +558,59 @@ namespace Server.SkillHandlers
 			AddRow(x, ref y, "Mood", loyalty);
 		}
 
-		private void AddFoodSection(int x, ref int y, BaseCreature c, int source)
+		private void AddFoodSection(int x, ref int y )
 		{
-			if ( (c.FavoriteFood & FoodType.None) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.None) != 0 )
 				AddRow(x, ref y, "None", "");
-			if ( (c.FavoriteFood & FoodType.FruitsAndVegies) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.FruitsAndVegies) != 0 )
 			{
 				AddRow(x, ref y, "Fruits", "");
 				AddRow(x, ref y, "Vegetables", "");
 			}
-			if ( (c.FavoriteFood & FoodType.GrainsAndHay) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.GrainsAndHay) != 0 )
 				AddRow(x, ref y, "Grains & Hay", "");
-			if ( (c.FavoriteFood & FoodType.Fish) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.Fish) != 0 )
 				AddRow(x, ref y, "Fish", "");
-			if ( (c.FavoriteFood & FoodType.Meat) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.Meat) != 0 )
 				AddRow(x, ref y, "Meat", "");
-			if ( (c.FavoriteFood & FoodType.Eggs) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.Eggs) != 0 )
 				AddRow(x, ref y, "Eggs", "");
-			if ( (c.FavoriteFood & FoodType.Gold) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.Gold) != 0 )
 				AddRow(x, ref y, "Gold", "");
-			if ( (c.FavoriteFood & FoodType.Fire) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.Fire) != 0 )
 			{
 				AddRow(x, ref y, "Brimstone", "");
 				AddRow(x, ref y, "Sulfurous Ash", "");
 			}
-			if ( (c.FavoriteFood & FoodType.Gems) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.Gems) != 0 )
 				AddRow(x, ref y, "Gems", "");
-			if ( (c.FavoriteFood & FoodType.Nox) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.Nox) != 0 )
 			{
 				AddRow(x, ref y, "Swamp Berries", "");
 				AddRow(x, ref y, "Nox Crystals", "");
 				AddRow(x, ref y, "Nightshade", "");
 			}
-			if ( (c.FavoriteFood & FoodType.Sea) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.Sea) != 0 )
 			{
 				AddRow(x, ref y, "Seaweed", "");
 				AddRow(x, ref y, "Sea Salt", "");
 			}
-			if ( (c.FavoriteFood & FoodType.Moon) != 0 )
+			if ( ( m_Creature.FavoriteFood & FoodType.Moon) != 0 )
 				AddRow(x, ref y, "Moon Crystals", "");
 		}
 
-        private void AddInformationSection( int x, ref int y, BaseCreature c, int source)
+        private void AddInformationSection( int x, ref int y )
         {
-			AddRow(x, ref y, "Power Level", IntelligentAction.GetCreatureLevel( c ).ToString());
+			AddRow(x, ref y, "Power Level", IntelligentAction.GetCreatureLevel( m_Creature ).ToString());
 
-			double bd = !c.Uncalmable ? Items.BaseInstrument.GetBaseDifficulty( c ) : 0;
+			double bd = !m_Creature.Uncalmable ? Items.BaseInstrument.GetBaseDifficulty( m_Creature ) : 0;
 			AddRow(x, ref y, "Barding Difficulty", FormatTalent( bd ));
 
-			if ( source == 0 && c.Tamable && c.MinTameSkill > 0 )
+			if ( m_Source == 0 && m_Creature.Tamable && m_Creature.MinTameSkill > 0 )
 			{
-				AddRow(x, ref y, "Taming Needed", FormatTaming( c.MinTameSkill ));
-				AddRow(x, ref y, "Follower Slots", c.ControlSlots.ToString());
+				AddRow(x, ref y, "Taming Needed", FormatTaming( m_Creature.MinTameSkill ));
+				AddRow(x, ref y, "Follower Slots", m_Creature.ControlSlots.ToString());
+				AddRow(x, ref y, "Sex", m_Creature.SexString);
 			}
         }
     }
