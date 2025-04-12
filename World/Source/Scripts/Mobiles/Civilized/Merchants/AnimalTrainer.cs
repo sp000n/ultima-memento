f306f63ac21c4e1aac5712e82c08a42669542657
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-04-12 09:45:53 -0400

    - Fix an issue where some pack animal types could be stabled with items

--- World/Source/Scripts/Mobiles/Civilized/Merchants/AnimalTrainer.cs
+++ World/Source/Scripts/Mobiles/Civilized/Merchants/AnimalTrainer.cs
@@ -577,7 +577,7 @@ namespace Server.Mobiles
 			{
 				SayTo( from, 502673 ); // I can not stable summoned creatures.
 			}
-			else if ( (pet is PackLlama || pet is PackHorse || pet is Beetle) && (pet.Backpack != null && pet.Backpack.Items.Count > 0) )
+			else if ( pet.Backpack != null && pet.Backpack is StrongBackpack && pet.Backpack.Items.Count > 0 )
 			{
 				SayTo( from, 1042563 ); // You need to unload your pet.
 			}
--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -67,6 +67,7 @@ namespace Server.Misc
 			builder.Append("- Misc - Treasure hoards now properly limit item attributes to 6<br>");
 			builder.Append("- Mobs - Fix an issue where mobs cast curse too much<br>");
 			builder.Append("- Pets - Fix an issue where mobs didn't stop healing when dead<br>");
+			builder.Append("- Pets - Fix an issue where some pack animal types could be stabled with items<br>");
 			builder.Append("- Quest - Fix null ref crash after [buildworld and restart<br>");
 			builder.Append("- Spell - Chain Lightning no longer does doubles damage to one target<br>");
 			builder.Append(SEPARATOR_LINE);
