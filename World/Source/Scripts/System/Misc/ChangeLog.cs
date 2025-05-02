c5a2ba8d26c5aced527a947d1467333f1805ce33
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-05-02 19:21:33 -0400

    - Witch Brewing now takes 4 total reagents, up from 2

--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -21,6 +21,7 @@ namespace Server.Misc
 
 			builder.Append("Changes<br>");
 			builder.Append("- Craft - Druidic Herbalism now takes 4 total reagents, up from 2<br>");
+			builder.Append("- Craft - Witch Brewing now takes 4 total reagents, up from 2<br>");
 			builder.Append("- Shoppe - Targeting a container now adds all possible items to the Order<br>");
 
 			builder.Append("<br>");
