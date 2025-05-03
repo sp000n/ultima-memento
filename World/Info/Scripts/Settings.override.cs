1cec5645d5fe163d219ec9c1dfe95227699cd9d3
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-05-03 08:58:00 -0400

    - Switch to Tabs

--- World/Info/Scripts/Settings.override.cs
+++ World/Info/Scripts/Settings.override.cs
@@ -1,15 +1,15 @@
 namespace Server
 {
-    /// <summary>
-    /// You may apply explicit overrides `MySettings` overrides here.
-    /// This file will never be touched by Memento development.
-    /// </summary>
+	/// <summary>
+	/// You may apply explicit overrides `MySettings` overrides here.
+	/// This file will never be touched by Memento development.
+	/// </summary>
 	public static class SettingOverrides
 	{
-        public static void Initialize()
-        {
-            // Example:
-            // MySettings.S_SaveOnCharacterLogout = true;
-        }
-    }
+		public static void Initialize()
+		{
+			// Example:
+			// MySettings.S_SaveOnCharacterLogout = true;
+		}
+	}
 }
