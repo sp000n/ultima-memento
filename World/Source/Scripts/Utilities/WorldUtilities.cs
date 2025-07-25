f78ba5331ddb7ab204083b95ed650f49d755a38f
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-07-25 14:06:42 -0400

    - dd function for grabbing all World items of a certain type

--- World/Source/Scripts/Utilities/WorldUtilities.cs
+++ World/Source/Scripts/Utilities/WorldUtilities.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Server.Utilities
@@ -14,6 +15,18 @@ namespace Server.Utilities
 				toDelete
 					.ToList()
 					.ForEach(item => item.Delete());
+			}
+		}
+
+		public static IEnumerable<T> ForEachItem<T>(Func<T, bool> predicate) where T : Item
+		{
+			var items = World.Items.Values
+				.Where(item => item is T && predicate((T)item));
+			if (!items.Any()) yield break;
+
+			foreach (var item in items.ToList())
+			{
+				yield return (T)item;
 			}
 		}
 	}
