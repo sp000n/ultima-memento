7300530a829374b90f39054c0a312d5dcb67ebb3
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-07-27 15:07:09 -0400

    - Add missed change

--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -18,6 +18,7 @@ namespace Server.Misc
 			builder.Append("Changes<br>");
 			builder.Append("- Item - Acidic robe cooldown reduced to 10 minutes (will be reworked in a future update)<br>");
 			builder.Append("- Item - High fame mobs can now drop Champion Skulls<br>");
+			builder.Append("- Misc - Champion spawns are now added to the world<br>");
 			builder.Append("- Mobs - Players are now immune to peacemaking for 30s after it ends<br>");
 			builder.Append("- NPC - Mercantile/guild barter bonus is reduced to +100% of item value, down from +300%<br>");
 			builder.Append("- NPC - Mercantile/guild bonus now increases the amount of Gold that vendors have available for purchase<br>");
