8bb4cca1ea14f9e28e2fff202a98c53b79d6ee31
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-08-01 18:20:16 -0400

    - Quest pedestal generation is now more resilient

--- World/Source/Scripts/System/Misc/ChangeLog.cs
+++ World/Source/Scripts/System/Misc/ChangeLog.cs
@@ -22,6 +22,7 @@ namespace Server.Misc
 			builder.Append("- Item - Lawn tools can now be used indoors<br>");
 			builder.Append("- Misc - Champion spawns are now added to the world<br>");
 			builder.Append("- Misc - Jewelry trap has been reworked to be less aggressive<br>");
+			builder.Append("- Misc - Quest pedestal generation is now more resilient<br>");
 			builder.Append("- Mobs - Players are now immune to peacemaking for 30s after it ends<br>");
 			builder.Append("- NPC - Mercantile/guild barter bonus is reduced to +100% of item value, down from +300%<br>");
 			builder.Append("- NPC - Mercantile/guild bonus now increases the amount of Gold that vendors have available for purchase<br>");
