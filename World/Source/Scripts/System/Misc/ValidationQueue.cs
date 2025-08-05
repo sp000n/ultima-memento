5f6de2e7835ccc3a0ce51a7f9db4d97461a242a5
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-08-05 18:42:45 -0400

    - Add validation queue to execute after everything is loaded

---
+++ World/Source/Scripts/System/Misc/ValidationQueue.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+namespace Server
+{
+	public interface IValidate
+	{
+		void Validate();
+	}
+
+	public delegate void ValidationEventHandler();
+
+	public static class ValidationQueue
+	{
+		public static event ValidationEventHandler StartValidation;
+
+		public static void Initialize()
+		{
+			if ( StartValidation != null )
+				StartValidation();
+
+			StartValidation = null;
+		}
+	}
+
+	public static class ValidationQueue<T> where T : IValidate
+	{
+		private static List<T> m_Queue;
+
+		static ValidationQueue()
+		{
+			m_Queue = new List<T>();
+			ValidationQueue.StartValidation += new ValidationEventHandler( ValidateAll );
+		}
+
+		public static void Add( T obj )
+		{
+			m_Queue.Add( obj );
+		}
+
+		private static void ValidateAll()
+		{
+			if (m_Queue == null) return;
+
+			for ( int i = 0; i < m_Queue.Count; i++ )
+			{
+				var item = m_Queue[i];
+				item.Validate();
+			}
+
+			m_Queue.Clear();
+			m_Queue = null;
+		}
+	}
+}
