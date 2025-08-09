93e05802cf250557902dbfa1d68a19659cad2fa5
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-08-09 19:36:15 -0400

    -

--- World/Source/Scripts/Utilities/ItemUtilities.cs
+++ World/Source/Scripts/Utilities/ItemUtilities.cs
@@ -2,21 +2,21 @@ using Server.Items;
 
 namespace Server.Utilities
 {
-    public static class ItemUtilities
-    {
-        public static bool IsExceptional(Item item)
-        {
-            if (item == null) return false;
+	public static class ItemUtilities
+	{
+		public static bool IsExceptional(Item item)
+		{
+			if (item == null) return false;
 
-            if (item is BaseWeapon) return ((BaseWeapon)item).Quality == WeaponQuality.Exceptional;
-            if (item is BaseArmor) return ((BaseArmor)item).Quality == ArmorQuality.Exceptional;
-            if (item is BaseClothing) return ((BaseClothing)item).Quality == ClothingQuality.Exceptional;
-            if (item is BaseInstrument) return ((BaseInstrument)item).Quality == InstrumentQuality.Exceptional;
-            if (item is BaseInstrument) return ((BaseInstrument)item).Quality == InstrumentQuality.Exceptional;
-            if (item is BaseTool) return ((BaseTool)item).Quality == ToolQuality.Exceptional;
-            if (item is BaseHarvestTool) return ((BaseHarvestTool)item).Quality == ToolQuality.Exceptional;
+			if (item is BaseWeapon) return ((BaseWeapon)item).Quality == WeaponQuality.Exceptional;
+			if (item is BaseArmor) return ((BaseArmor)item).Quality == ArmorQuality.Exceptional;
+			if (item is BaseClothing) return ((BaseClothing)item).Quality == ClothingQuality.Exceptional;
+			if (item is BaseInstrument) return ((BaseInstrument)item).Quality == InstrumentQuality.Exceptional;
+			if (item is BaseInstrument) return ((BaseInstrument)item).Quality == InstrumentQuality.Exceptional;
+			if (item is BaseTool) return ((BaseTool)item).Quality == ToolQuality.Exceptional;
+			if (item is BaseHarvestTool) return ((BaseHarvestTool)item).Quality == ToolQuality.Exceptional;
 
-            return false;
-        }
-    }
+			return false;
+		}
+	}
 }
